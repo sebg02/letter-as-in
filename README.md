ffcd026002553a54184649490b70f5ed412ec957
sebg02 <103606314+sebg02@users.noreply.github.com>
2023-05-29 21:50:48 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Página Alfabeto Fonético de la OTAN
+
+Este página te permite ingresar una o varias palabras y muestra el equivalente del Alfabeto Fonético de la OTAN para cada letra de la palabra, además de otras palabras que también inician con esta letra tanto en inglés como en español. Es útil para comunicaciones claras y precisas, especialmente en entornos de telecomunicación como llamadas telefónicas.
+
+Características principales:
+
+- ✅ Convierte cada letra de la palabra ingresada en su equivalente del Alfabeto Fonético de la OTAN.
+- ✅ Proporciona una forma estandarizada de comunicar letras individualmente para evitar errores y malentendidos.
+- ✅ Ayuda a mejorar la claridad y la comprensión en situaciones donde la comunicación precisa es crucial.
+- ✅ Ejemplos en inglés y en Español.
+
+El alfabeto fonético de la OTAN es un alfabeto ortográfico, un conjunto de palabras que se utilizan en lugar de letras en la comunicación oral (es decir, por teléfono o por radio militar). Cada palabra ("palabra clave") representa su letra inicial ("símbolo" alfabético). Las 26 palabras clave del alfabeto fonético de la OTAN se asignan a las 26 letras del alfabeto inglés.
+
+---
+
+# NATO Phonetic Alphabet Page
+
+This page allows you to enter one or more words and shows the NATO Phonetic Alphabet equivalent for each letter in the word, as well as other words that also start with this letter in both English and Spanish. It is useful for clear and precise communications, especially in telecommunication environments such as phone calls.
+
+Main features:
+
+- ✅ Converts each letter of the entered word into its equivalent from the NATO Phonetic Alphabet.
+- ✅ Provides a standardized way to communicate letters individually to avoid mistakes and misunderstandings.
+- ✅ Helps improve clarity and understanding in situations where precise communication is crucial.
+- ✅ Examples in English and Spanish.
+
+The NATO Phonetic Alphabet is a spelling alphabet, a set of words used in place of letters in oral communication (i.e., by telephone or military radio). Each word ("keyword") represents its initial letter (alphabetic "symbol"). The 26 code words of the NATO phonetic alphabet map to the 26 letters of the English alphabet.
---
+++ main.js
@@ -0,0 +1,81 @@
+const letter = {
+  a: "alfa	apple	airplane		arbol	abeja",
+  b: "bravo	bingo	bus		boca	barco",
+  c: "charlie	cat	carrot		casa	cocina",
+  d: "delta	dog	duck		doctor	delfin",
+  e: "Echo	economy	elephant		estrella	elefante",
+  f: "Foxtrot	face	fish		flor	fruta",
+  g: "golf	garden	guitar		gato	gallina",
+  h: "hotel	happy	horse		hojas	hielo",
+  i: "india	idea	iron isla invierno",
+  j: "juliett	jacket	jeans		jirafa	jugo",
+  k: "kilo	kitchen	ketchup		kilo	koala",
+  l: "lima	lake	lamp		lapiz	libro",
+  m: "mike	mango	moon		mango	mesa",
+  n: "november 	nature	native		noche	nube",
+  o: "oscar	orange	ocean		oso	oreja",
+  p: "papa	pie	parrot		pato	pelota",
+  q: "quebec	queen	quiz		Queso	quimica",
+  r: "romeo	rocket	robot		raton	rio",
+  s: "sierra	sun	spoon		sombrero	sapo",
+  t: "tango 	truck	tiger		tiburon	tambor",
+  u: "uniform	umbrella	use		uva	union",
+  v: "victor	violin	voice		vaca	vela",
+  w: "whiskey	wolf	window		waterpolo	wikipedia",
+  x: "xray	xerox	xilophone		xilofono	xiaomi",
+  y: "yankee	yogurt	yellow		yunque	yate",
+  z: "zulu	zebra	zoo		zapato	zorro",
+  ñ: "-	-	-		ñoño	ñandu",
+  " ": "<br>"
+};
+
+const default_letter = "Not valid character";
+
+function changeAcents(str) {
+  const acentos = {
+    á: "a",
+    é: "e",
+    í: "i",
+    ó: "o",
+    ú: "u",
+    Á: "A",
+    É: "E",
+    Í: "I",
+    Ó: "O",
+    Ú: "U"
+  };
+  return str
+    .split("")
+    .map((letra) => acentos[letra] || letra)
+    .join("")
+    .toString();
+}
+
+function spellText() {
+  document.getElementById("outcome_field").innerHTML = "";
+  let text = document.getElementById("user-input").value;
+  text = changeAcents(text);
+
+  for (let i of text) {
+    let outcome = letter[i] || default_letter;
+    let toshow = "";
+    toshow =
+      "<b>" + i.toUpperCase() + "</b>" + " as in " + outcome.toUpperCase();
+    if (i == " ") {
+      toshow = "<br>";
+    }
+    let newp = document.createElement("p");
+    newp.innerHTML = toshow;
+    document.getElementById("outcome_field").appendChild(newp);
+  }
+}
+
+let input = document.getElementById("user-input");
+
+input.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    spellText();
+    document.getElementById("user-input").value = "";
+    input.blur(); // hide keyboard when pressed enter in mobiles
+  }
+});
